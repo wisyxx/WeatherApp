62870b20f1761aa273d2053a267e0c78489942ba
Álvaro Hernández <alvarohcolino@gmail.com>
2024-09-15 18:05:14 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+
+export const App = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Weather App</h1>ç
+
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </>
+  );
+};
